cdcde386cb140cdec9d56d2a2e67e723f0d8d9ea
Samantha Divina <lavcorps@protonmail.com>
2026-05-26 16:59:48 -0600

    implement PlayerCommandContext.ts

---
+++ Classes/PlayerCommandContext.ts
@@ -0,0 +1,80 @@
+// SPDX-FileCopyrightText: 2026 LavCorps <lavcorps@protonmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+import type { Collection } from "discord.js";
+import type Exit from "../Data/Exit.ts";
+import type Fixture from "../Data/Fixture.ts";
+import type InventoryItem from "../Data/InventoryItem.ts";
+import type Player from "../Data/Player.ts";
+import type Room from "../Data/Room.ts";
+import type RoomItem from "../Data/RoomItem.ts";
+import type Game from "../Data/Game.ts";
+import type EquipmentSlot from "../Data/EquipmentSlot.ts";
+
+/**
+ * Represents the in-game context of a command.
+ */
+export default class Context {
+    /**
+     * The game containing all objects of this context.
+     */
+    private readonly game: Game
+    
+    /**
+     * The player responsible for executing the command.
+     */
+    readonly player: Player
+    
+    /**
+     * The equipment slots of the player.
+     */
+    readonly equipmentSlots: Collection<string, EquipmentSlot>
+    
+    /**
+     * The inventory items held by the player.
+     */
+    readonly inventoryItems: Array<InventoryItem>
+    
+    /**
+     * The room the player occupies.
+     */
+    readonly room: Room
+    
+    /**
+     * The other players in the same room as the player.
+     */
+    readonly players: Array<Player>
+    
+    /**
+     * The exits within the room.
+     */
+    readonly exits: Collection<string, Exit>
+    
+    /**
+     * The fixtures within the room.
+     */
+    readonly fixtures: Array<Fixture>
+    
+    /**
+     * The room items within the room.
+     */
+    readonly roomItems: Array<RoomItem>
+    
+    /**
+     * 
+     * @param game - The game to construct the context within.
+     * @param player - The player to construct the context for.
+     */
+    private constructor(game: Game, player: Player) {
+        this.game = game;
+        this.player = player;
+        this.equipmentSlots = this.player.inventory;
+        this.inventoryItems = this.equipmentSlots.filter(slot => !slot.containsNoItems()).map(slot => slot.equippedItem)
+        this.room = this.player.location;
+        this.players = this.room.occupants.filter(roomPlayer => roomPlayer !== this.player)
+        this.exits = this.room.exits;
+        this.fixtures = this.game.entityFinder.getFixtures(null, this.room.id);
+        this.roomItems = this.room.getContainedItems()
+    }
+}
