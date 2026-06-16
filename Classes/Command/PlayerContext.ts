941ca1fd6a006802d794622b77bab9ca7489c272
LavCorps <lavcorps@protonmail.com>
2026-06-16 00:44:32 -0600

    implement PlayerContext.getLexicon()

--- Classes/Command/PlayerContext.ts
+++ Classes/Command/PlayerContext.ts
@@ -14,6 +14,8 @@ import type RoomItem from "../../Data/RoomItem.ts";
 import type Game from "../../Data/Game.ts";
 import type EquipmentSlot from "../../Data/EquipmentSlot.ts";
 import Context from "./Context.ts";
+import { EntityToken, ItemContainerToken, PrepositionToken, type Token } from "./Token.ts";
+import type Puzzle from "../../Data/Puzzle.ts";
 
 /**
  * Represents the in-game context of a new-generation player command.
@@ -80,9 +82,19 @@ export default class PlayerContext extends Context {
     readonly exits: Collection<string, Exit>;
 
     /**
+     * The rooms adjacent to the player's location.
+     */
+    readonly adjacentRooms: Array<Room>;
+
+    /**
      * The fixtures within the room.
      */
     readonly fixtures: Array<Fixture>;
+
+    /**
+     * The puzzles within the room.
+     */
+    readonly puzzles: Array<Puzzle>;
 
     /**
      * The room items within the room.
@@ -114,7 +126,70 @@ export default class PlayerContext extends Context {
         this.room = this.player.location;
         this.players = this.room.occupants.filter((roomPlayer) => roomPlayer !== this.player);
         this.exits = this.room.exits;
-        this.fixtures = this.game.entityFinder.getFixtures(null, this.room.id);
+        this.adjacentRooms = this.exits.map((exit) => exit.dest);
+        this.fixtures = this.game.entityFinder.getFixtures(null, this.room.id).filter((fixture) => fixture.accessible);
+        this.puzzles = this.game.entityFinder.getPuzzles(null, this.room.id).filter((puzzle) => puzzle.accessible);
         this.roomItems = this.room.getContainedItems();
+    }
+
+    getLexicon(): Token[] {
+        const prepositions: Set<string> = new Set();
+        const tokens: Token[] = [];
+
+        for (const player of this.game.players.values()) {
+            tokens.push(new EntityToken(player.displayName, player));
+        }
+
+        for (const item of this.inventoryItems) {
+            if (item.prefab !== null && item.quantity > 0) {
+                const preposition = item.getPreposition();
+                tokens.push(new ItemContainerToken(item.name, item));
+                if (item.name !== item.pluralName) tokens.push(new ItemContainerToken(item.pluralName, item));
+                if (!prepositions.has(preposition)) {
+                    prepositions.add(preposition);
+                    tokens.push(new PrepositionToken(preposition));
+                }
+            }
+        }
+
+        for (const item of this.roomItems) {
+            if (item.prefab !== null && item.quantity > 0) {
+                const preposition = item.getPreposition();
+                tokens.push(new ItemContainerToken(item.name, item));
+                if (item.name !== item.pluralName) tokens.push(new ItemContainerToken(item.pluralName, item));
+                if (!prepositions.has(preposition)) {
+                    prepositions.add(preposition);
+                    tokens.push(new PrepositionToken(preposition));
+                }
+            }
+        }
+
+        for (const fixture of this.fixtures) {
+            const preposition = fixture.getPreposition();
+            tokens.push(new ItemContainerToken(fixture.name, fixture));
+            if (!prepositions.has(preposition)) {
+                prepositions.add(preposition);
+                tokens.push(new PrepositionToken(preposition));
+            }
+        }
+
+        for (const puzzle of this.puzzles) {
+            const preposition = puzzle.getPreposition();
+            tokens.push(new ItemContainerToken(puzzle.name, puzzle));
+            if (!prepositions.has(preposition)) {
+                prepositions.add(preposition);
+                tokens.push(new PrepositionToken(preposition));
+            }
+        }
+
+        for (const room of this.adjacentRooms) {
+            tokens.push(new EntityToken(room.id, room));
+        }
+
+        for (const exit of this.exits.values()) {
+            tokens.push(new EntityToken(exit.name, exit));
+        }
+
+        return tokens;
     }
 }
