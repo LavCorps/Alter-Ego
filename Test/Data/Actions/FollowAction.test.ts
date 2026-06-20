eebc7aaf068fefdf184abb9843bd0bd0df52b53f
Ms. VBLANK <alteregomolly@pm.me>
2026-06-20 17:57:55 -0400

    Added test stubs for Party Actions.

---
+++ Test/Data/Actions/FollowAction.test.ts
@@ -0,0 +1,7 @@
+// SPDX-FileCopyrightText: 2026 Ms. VBLANK <alteregomolly@pm.me>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+describe('FollowAction test', () => {
+    test('', () => {});
+});
