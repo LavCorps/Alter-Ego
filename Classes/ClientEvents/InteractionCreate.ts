8704892421541b334d8e969eec1b2765c3a1d888
Ms. VBLANK <alteregomolly@pm.me>
2026-06-21 16:22:08 -0400

    Refactored InteractionCreate event.

---
+++ Classes/ClientEvents/InteractionCreate.ts
@@ -0,0 +1,16 @@
+// SPDX-FileCopyrightText: 2026 Ms. VBLANK <alteregomolly@pm.me>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+import { Events, type Interaction } from "discord.js";
+import ClientEvent from "./ClientEvent.ts";
+
+export default new ClientEvent({
+    name: Events.InteractionCreate,
+    once: false,
+    execute: async (interaction: Interaction) => {
+        if (!ClientEvent.clientReady) return;
+
+        ClientEvent.client.interactionHandler.interceptInteraction(interaction);
+    }
+});
