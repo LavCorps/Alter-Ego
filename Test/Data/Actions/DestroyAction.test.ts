78502446becf69f912ef2d74eb2b46d8cc22a59c
LavCorps <lavcorps@protonmail.com>
2026-06-26 15:29:14 -0600

    remove empty action tests

--- Test/Data/Actions/DestroyAction.test.ts
+++
@@ -1,7 +0,0 @@
-// SPDX-FileCopyrightText: 2019 Alter Ego Contributors
-//
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
-describe('DestroyAction test', () => {
-    test('', () => {});
-});
--- Test/Data/Actions/StopFollowingAction.test.ts
+++
@@ -1,7 +0,0 @@
-// SPDX-FileCopyrightText: 2026 Ms. VBLANK <alteregomolly@pm.me>
-//
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
-describe('StopFollowingAction test', () => {
-    test('', () => {});
-});
