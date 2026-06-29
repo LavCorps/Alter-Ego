ae7d3ed066adf699bb4e87916211f2d9a35c882f
Ms. VBLANK <alteregomolly@pm.me>
2026-06-29 17:23:50 -0400

    Made DismissAction asynchronous.

--- Data/Actions/DismissAction.ts
+++ Data/Actions/DismissAction.ts
@@ -5,21 +5,39 @@
 import Action from "../Action.ts";
 import type Player from "../Player.ts";
 import { generateListString } from "../../Modules/helpers.ts";
+import DisbandPartyAction from "./DisbandPartyAction.ts";
 
 /**
  * Represents a dismiss action.
- * 
+ *
  * @see https://msvblank.github.io/Alter-Ego/reference/data_structures/action.html#dismiss-action
  */
 export default class DismissAction extends Action {
-    performDismissAction(followers: Player[], stopFollowing: boolean = false): void {
+    /**
+     * Performs a dismiss action.
+     *
+     * @param followers - The followers to dismiss.
+     * @param stopFollowing - Whether or not all of the listed followers should stop following the leader. Defaults to false.
+     */
+    async performDismissAction(followers: Player[], stopFollowing: boolean = false): Promise<void> {
         if (this.performed) return;
         super.perform();
         const party = this.player.party;
+
+        // If all followers are being dismissed, disband the party instead.
+        const disband = party.followers.size === followers.length && party.followers.every(follower => followers.includes(follower));
+        if (disband) {
+            const disbandAction = new DisbandPartyAction(this.getGame(), this.message, this.player, this.location, this.forced, this.whisper, this.user);
+            await disbandAction.performDisbandParty(stopFollowing);
+            this.successMessage = disbandAction.successMessage;
+            return;
+        }
+
+        // Otherwise, just dismiss the listed players.
         this.getGame().narrationHandler.narrateDismiss(this, this.player, followers);
         for (const follower of followers) {
             this.player.stopLeading(follower);
-            party.removeFollower(follower, this, this.getGame().notificationGenerator.generateDismissNotification(this.player, false, party.getMemberDisplayName(follower)));
+            await party.removeFollower(follower, this, this.getGame().notificationGenerator.generateDismissNotification(this.player, false, party.getMemberDisplayName(follower)));
             if (stopFollowing) follower.stopFollowing();
         }
         this.successMessage = `Successfully dismissed ${generateListString(followers.map(follower => follower.name))} from ${this.player.name}'s party.`;
--- Test/Data/Actions/DismissAction.test.ts
+++ Test/Data/Actions/DismissAction.test.ts
@@ -2,6 +2,309 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+import Player from "../../../Data/Player.ts";
+import type Status from "../../../Data/Status.ts";
+import DisbandPartyAction from "../../../Data/Actions/DisbandPartyAction.ts";
+import DismissAction from "../../../Data/Actions/DismissAction.ts";
+import FollowAction from "../../../Data/Actions/FollowAction.ts";
+import LeadAction from "../../../Data/Actions/LeadAction.ts";
+import GameEntityManager from "../../../Classes/GameEntityManager.ts";
+import { sendQueuedMessages } from "../../../Modules/messageHandler.ts";
+import type { Mock } from "vitest";
+import type { Message } from "discord.js";
+
 describe('DismissAction test', () => {
-    test('', () => {});
+    /**
+     * Location: lobby
+     *
+     * Speed: 10
+     */
+    let astrid: Player;
+    /**
+     * Speed: 5
+     */
+    let asuka: Player;
+    /**
+     * Location: lobby
+     *
+     * Speed: 1
+     */
+    let nero: Player;
+    /**
+     * Inflicts spd+4, getting Astrid to the speed we need her at.
+     */
+    let fast: Status;
+    /**
+     * Changes Astrid's display name.
+     */
+    let concealed: Status;
+    /**
+     * The concealed display name to set for Astrid.
+     */
+    let concealedDisplayName: string;
+    /**
+     * Inflicts spd+1, getting Asuka to the speed we need her at.
+     */
+    let cheerful: Status;
+    /**
+     * Inflicts spd-4, getting Nero to the speed we need him at.
+     */
+    let crutches: Status;
+
+    beforeAll(async () => {
+        if (!game.inProgress) await game.entityLoader.loadAll();
+        astrid = game.entityFinder.getLivingPlayer("Astrid");
+        asuka = game.entityFinder.getLivingPlayer("Asuka");
+        nero = game.entityFinder.getLivingPlayer("Nero");
+        fast = game.entityFinder.getStatusEffect("fast");
+        concealed = game.entityFinder.getStatusEffect("concealed");
+        cheerful = game.entityFinder.getStatusEffect("cheerful");
+        crutches = game.entityFinder.getStatusEffect("crutches");
+        astrid.inflict(fast);
+        astrid.inflict(concealed);
+        concealedDisplayName = "an individual wearing a MASK";
+        astrid.setPronouns(astrid.pronouns, "neutral");
+        astrid.displayName = concealedDisplayName;
+        asuka.inflict(cheerful);
+        nero.inflict(crutches);
+    });
+
+    afterAll(() => {
+        astrid.cure(fast);
+        astrid.cure(concealed);
+        astrid.displayName = "Astrid";
+        astrid.setPronouns(astrid.pronouns, astrid.pronounString);
+        asuka.cure(cheerful);
+        nero.cure(crutches);
+    });
+
+    test('Setup is correct', () => {
+        expect(astrid.speed).toBe(10);
+        expect(asuka.speed).toBe(5);
+        expect(nero.speed).toBe(1);
+        expect(astrid.location.id).toBe("lobby");
+        expect(asuka.location.id).toBe("lobby");
+        expect(nero.location.id).toBe("lobby");
+    });
+
+    describe('DismissAction.performDismiss tests', () => {
+        let deletePartySpy: Mock<typeof GameEntityManager.prototype.deleteParty>;
+        let narrationMessage: Message<boolean>;
+        let astridNotificationMessage: Message<boolean>;
+        let asukaNotificationMessage: Message<boolean>;
+        let neroNotificationMessage: Message<boolean>;
+
+        const clearMessages = () => {
+            astrid.location.channel.messages.cache.clear();
+            astrid.notificationChannel.messages.cache.clear();
+            asuka.notificationChannel.messages.cache.clear();
+            nero.notificationChannel.messages.cache.clear();
+        };
+
+        const sendMessages = async () => {
+            await sendQueuedMessages(game);
+            narrationMessage = astrid.location.channel.messages.cache.last();
+            astridNotificationMessage = astrid.notificationChannel.messages.cache.last();
+            asukaNotificationMessage = asuka.notificationChannel.messages.cache.last();
+            neroNotificationMessage = nero.notificationChannel.messages.cache.last();
+        }
+
+        beforeEach(async () => {
+            deletePartySpy = vi.spyOn(GameEntityManager.prototype, 'deleteParty');
+            const followAction1 = new FollowAction(game, undefined, asuka, asuka.location, false);
+            await followAction1.performFollow(astrid);
+            const followAction2 = new FollowAction(game, undefined, nero, nero.location, false);
+            await followAction2.performFollow(astrid);
+            const leadAction = new LeadAction(game, undefined, astrid, astrid.location, false);
+            await leadAction.performLead([asuka, nero]);
+            await sendMessages();
+            clearMessages();
+        });
+
+        afterEach(async () => {
+            const disbandAction = new DisbandPartyAction(game, undefined, astrid, astrid.location, false);
+            await disbandAction.performDisbandParty(true);
+            await sendMessages();
+            clearMessages();
+            vi.restoreAllMocks();
+        });
+
+        test('party setup is correct', () => {
+            expect(astrid.party).not.toBeNull();
+            expect(astrid.party.members).toHaveSize(3);
+            expect(astrid.party.hasLeader(astrid)).toBe(true);
+            expect(astrid.party.hasFollower(asuka)).toBe(true);
+            expect(astrid.party.hasFollower(nero)).toBe(true);
+            expect(game.parties.has(astrid.party.id)).toBe(true);
+            expect(game.whispers.has(astrid.party.id)).toBe(true);
+        });
+
+        test('dismiss one follower and do not stop following', async () => {
+            const dismissAction = new DismissAction(game, undefined, astrid, astrid.location, false);
+            await dismissAction.performDismissAction([nero], false);
+
+            expect(astrid.ledPlayers).toHaveLength(1);
+            expect(astrid.getLedPlayer("Asuka")).toStrictEqual(asuka);
+            expect(astrid.getLedPlayer("Nero")).toBeNull();
+            expect(astrid.isLeading(asuka)).toBe(true);
+            expect(astrid.isLeading(nero)).toBe(false);
+            expect(deletePartySpy).not.toHaveBeenCalled();
+            expect(game.parties.has("party-an-individual-wearing-a-mask-asuka")).toBe(true);
+            expect(game.whispers.has("party-an-individual-wearing-a-mask-asuka")).toBe(true);
+            expect(astrid.party).not.toBeNull();
+            expect(asuka.party).not.toBeNull();
+            expect(nero.party).toBeNull();
+            expect(astrid.party).toStrictEqual(asuka.party);
+            expect(asuka.followedPlayer).toStrictEqual(astrid);
+            expect(nero.followedPlayer).toStrictEqual(astrid);
+            expect(asuka.followedPlayerDisplayName).toBe(concealedDisplayName);
+            expect(nero.followedPlayerDisplayName).toBe(concealedDisplayName);
+            expect(asuka.isFollowing(astrid)).toBe(true);
+            expect(nero.isFollowing(astrid)).toBe(true);
+            expect(astrid.viewParty(false)).toBe(`You are the leader of a party.\n\nAsuka is traveling together with you.`);
+            expect(asuka.viewParty(false)).toBe(`You are in a party led by ${concealedDisplayName}.`);
+            expect(nero.viewParty(false)).toBe(`You are not in a party. However, you are following ${concealedDisplayName}.`);
+            expect(astrid.viewParty(true)).toBe(`Astrid is the leader of a party.\n\nAsuka is traveling together with her.`);
+            expect(asuka.viewParty(true)).toBe(`Asuka is in a party led by Astrid.`);
+            expect(nero.viewParty(true)).toBe(`Nero is not in a party. However, he is following Astrid.`);
+
+            // Check all of the sent messages.
+            await sendMessages();
+            expect(astrid.location.channel.messages.cache).toHaveSize(1);
+            expect(narrationMessage.content).toBe(`> -# An individual wearing a MASK stops leading Nero.`);
+            expect(astrid.notificationChannel.messages.cache).toHaveSize(1);
+            expect(astridNotificationMessage.content).toBe(`You stop leading Nero.`);
+            expect(asuka.notificationChannel.messages.cache).toHaveSize(0);
+            expect(nero.notificationChannel.messages.cache).toHaveSize(1);
+            expect(neroNotificationMessage.content).toBe(`An individual wearing a MASK stops leading you.`);
+        });
+
+        test('dismiss all followers and do not stop following', async () => {
+            const dismissAction = new DismissAction(game, undefined, astrid, astrid.location, false);
+            await dismissAction.performDismissAction([nero, asuka], false);
+
+            expect(astrid.ledPlayers).toHaveLength(0);
+            expect(astrid.getLedPlayer("Asuka")).toBeNull();
+            expect(astrid.getLedPlayer("Nero")).toBeNull();
+            expect(astrid.isLeading(asuka)).toBe(false);
+            expect(astrid.isLeading(nero)).toBe(false);
+            expect(deletePartySpy).toHaveBeenCalledOnce();
+            expect(game.parties.has("party-an-individual-wearing-a-mask-asuka-nero")).toBe(false);
+            expect(game.parties.has("party-an-individual-wearing-a-mask-asuka")).toBe(false);
+            expect(game.parties.has("party-an-individual-wearing-a-mask")).toBe(false);
+            expect(game.whispers.has("party-an-individual-wearing-a-mask-asuka-nero")).toBe(false);
+            expect(game.whispers.has("party-an-individual-wearing-a-mask-asuka")).toBe(false);
+            expect(game.whispers.has("party-an-individual-wearing-a-mask")).toBe(false);
+            expect(astrid.party).toBeNull();
+            expect(asuka.party).toBeNull();
+            expect(nero.party).toBeNull();
+            expect(asuka.followedPlayer).toStrictEqual(astrid);
+            expect(nero.followedPlayer).toStrictEqual(astrid);
+            expect(asuka.followedPlayerDisplayName).toBe(concealedDisplayName);
+            expect(nero.followedPlayerDisplayName).toBe(concealedDisplayName);
+            expect(asuka.isFollowing(astrid)).toBe(true);
+            expect(nero.isFollowing(astrid)).toBe(true);
+            expect(astrid.viewParty(false)).toBe(`You are not in a party.`);
+            expect(asuka.viewParty(false)).toBe(`You are not in a party. However, you are following ${concealedDisplayName}.`);
+            expect(nero.viewParty(false)).toBe(`You are not in a party. However, you are following ${concealedDisplayName}.`);
+            expect(astrid.viewParty(true)).toBe(`Astrid is not in a party.`);
+            expect(asuka.viewParty(true)).toBe(`Asuka is not in a party. However, she is following Astrid.`);
+            expect(nero.viewParty(true)).toBe(`Nero is not in a party. However, he is following Astrid.`);
+
+            // Check all of the sent messages.
+            await sendMessages();
+            expect(astrid.location.channel.messages.cache).toHaveSize(1);
+            expect(narrationMessage.content).toBe(`> -# An individual wearing a MASK disbands their party, but Asuka and Nero are still following them.`);
+            expect(astrid.notificationChannel.messages.cache).toHaveSize(1);
+            expect(astridNotificationMessage.content).toBe(`You disband your party, but Asuka and Nero are still following you.`);
+            expect(asuka.notificationChannel.messages.cache).toHaveSize(1);
+            expect(asukaNotificationMessage.content).toBe(`An individual wearing a MASK has disbanded your party, but you are still following them.`);
+            expect(nero.notificationChannel.messages.cache).toHaveSize(1);
+            expect(neroNotificationMessage.content).toBe(`An individual wearing a MASK has disbanded your party, but you are still following them.`);
+        });
+
+        test('dismiss one follower and stop following', async () => {
+            const dismissAction = new DismissAction(game, undefined, astrid, astrid.location, false);
+            await dismissAction.performDismissAction([nero], true);
+
+            expect(astrid.ledPlayers).toHaveLength(1);
+            expect(astrid.getLedPlayer("Asuka")).toStrictEqual(asuka);
+            expect(astrid.getLedPlayer("Nero")).toBeNull();
+            expect(astrid.isLeading(asuka)).toBe(true);
+            expect(astrid.isLeading(nero)).toBe(false);
+            expect(deletePartySpy).not.toHaveBeenCalled();
+            expect(game.parties.has("party-an-individual-wearing-a-mask-asuka")).toBe(true);
+            expect(game.whispers.has("party-an-individual-wearing-a-mask-asuka")).toBe(true);
+            expect(astrid.party).not.toBeNull();
+            expect(asuka.party).not.toBeNull();
+            expect(nero.party).toBeNull();
+            expect(astrid.party).toStrictEqual(asuka.party);
+            expect(asuka.followedPlayer).toStrictEqual(astrid);
+            expect(nero.followedPlayer).toBeNull();
+            expect(asuka.followedPlayerDisplayName).toBe(concealedDisplayName);
+            expect(nero.followedPlayerDisplayName).toBe("");
+            expect(asuka.isFollowing(astrid)).toBe(true);
+            expect(nero.isFollowing(astrid)).toBe(false);
+            expect(astrid.viewParty(false)).toBe(`You are the leader of a party.\n\nAsuka is traveling together with you.`);
+            expect(asuka.viewParty(false)).toBe(`You are in a party led by ${concealedDisplayName}.`);
+            expect(nero.viewParty(false)).toBe(`You are not in a party.`);
+            expect(astrid.viewParty(true)).toBe(`Astrid is the leader of a party.\n\nAsuka is traveling together with her.`);
+            expect(asuka.viewParty(true)).toBe(`Asuka is in a party led by Astrid.`);
+            expect(nero.viewParty(true)).toBe(`Nero is not in a party.`);
+
+            // Check all of the sent messages.
+            await sendMessages();
+            expect(astrid.location.channel.messages.cache).toHaveSize(1);
+            expect(narrationMessage.content).toBe(`> -# An individual wearing a MASK stops leading Nero.`);
+            expect(astrid.notificationChannel.messages.cache).toHaveSize(1);
+            expect(astridNotificationMessage.content).toBe(`You stop leading Nero.`);
+            expect(asuka.notificationChannel.messages.cache).toHaveSize(0);
+            expect(nero.notificationChannel.messages.cache).toHaveSize(1);
+            expect(neroNotificationMessage.content).toBe(`An individual wearing a MASK stops leading you.`);
+        });
+
+        test('dismiss all followers and stop following', async () => {
+            const dismissAction = new DismissAction(game, undefined, astrid, astrid.location, false);
+            await dismissAction.performDismissAction([nero, asuka], true);
+
+            expect(astrid.ledPlayers).toHaveLength(0);
+            expect(astrid.getLedPlayer("Asuka")).toBeNull();
+            expect(astrid.getLedPlayer("Nero")).toBeNull();
+            expect(astrid.isLeading(asuka)).toBe(false);
+            expect(astrid.isLeading(nero)).toBe(false);
+            expect(deletePartySpy).toHaveBeenCalledOnce();
+            expect(game.parties.has("party-an-individual-wearing-a-mask-asuka-nero")).toBe(false);
+            expect(game.parties.has("party-an-individual-wearing-a-mask-asuka")).toBe(false);
+            expect(game.parties.has("party-an-individual-wearing-a-mask")).toBe(false);
+            expect(game.whispers.has("party-an-individual-wearing-a-mask-asuka-nero")).toBe(false);
+            expect(game.whispers.has("party-an-individual-wearing-a-mask-asuka")).toBe(false);
+            expect(game.whispers.has("party-an-individual-wearing-a-mask")).toBe(false);
+            expect(astrid.party).toBeNull();
+            expect(asuka.party).toBeNull();
+            expect(nero.party).toBeNull();
+            expect(asuka.followedPlayer).toBeNull();
+            expect(nero.followedPlayer).toBeNull();
+            expect(asuka.followedPlayerDisplayName).toBe("");
+            expect(nero.followedPlayerDisplayName).toBe("");
+            expect(asuka.isFollowing(astrid)).toBe(false);
+            expect(nero.isFollowing(astrid)).toBe(false);
+            expect(astrid.viewParty(false)).toBe(`You are not in a party.`);
+            expect(asuka.viewParty(false)).toBe(`You are not in a party.`);
+            expect(nero.viewParty(false)).toBe(`You are not in a party.`);
+            expect(astrid.viewParty(true)).toBe(`Astrid is not in a party.`);
+            expect(asuka.viewParty(true)).toBe(`Asuka is not in a party.`);
+            expect(nero.viewParty(true)).toBe(`Nero is not in a party.`);
+
+            // Check all of the sent messages.
+            await sendMessages();
+            expect(astrid.location.channel.messages.cache).toHaveSize(1);
+            expect(narrationMessage.content).toBe(`> -# An individual wearing a MASK disbands their party.`);
+            expect(astrid.notificationChannel.messages.cache).toHaveSize(1);
+            expect(astridNotificationMessage.content).toBe(`You disband your party.`);
+            expect(asuka.notificationChannel.messages.cache).toHaveSize(1);
+            expect(asukaNotificationMessage.content).toBe(`An individual wearing a MASK has disbanded your party. You are no longer following them.`);
+            expect(nero.notificationChannel.messages.cache).toHaveSize(1);
+            expect(neroNotificationMessage.content).toBe(`An individual wearing a MASK has disbanded your party. You are no longer following them.`);
+        });
+    });
 });
